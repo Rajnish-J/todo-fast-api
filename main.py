570f6e2bf7dd187263d6f65d67e10924366781be
Rajnish-J <rajnishofficial02@gmail.com>
2025-02-25 11:40:46 +0530

    REFACTOR: added some API's to check

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/home")
+def home():
+    return "Hello from home page"
+
 @app.get("/about")
-def greet():
-    return {"message": "Hello, World!"}
+def blog():
+    return "about page from the blog"
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def blog(id:int):
+    return {'data': id}
+
+
+@app.get("/blog/{id}/comments")
+def blogComments(id:int):
+    return {'data': {'good video', 'Fantastic teaching'}}
