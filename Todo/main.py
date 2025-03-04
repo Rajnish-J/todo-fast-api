24e3cb2e02f51d0c51ec70ff8fa0d601dc9733f0
Rajnish-J <rajnishofficial02@gmail.com>
2025-03-04 15:53:31 +0530

    FEAT: read by todo id api added, todorequest validations added
    HTTPException and status code set up done

--- Todo/main.py
+++ Todo/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 import models
 from models import Todo
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # * Create the database tables (Only needed if not using Alembic for migrations)
 models.Base.metadata.create_all(bind=engine)
@@ -21,13 +21,23 @@ def get_db():
 
 # * Pydantic model to validate incoming request data
 class TodoRequest(BaseModel):
-    title: str
-    description: str
-    priority: int
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
     complete_status: bool = False  # * Default value if not provided
 
 # * API Endpoint: To Read all the TODO's in the database
-@app.get("/")
+@app.get("/read_All_TODO", status_code=status.HTTP_200_OK)
 async def readAllTodo(db: Session = Depends(get_db)):
-    return db.query(Todo).all()
+    if db.query(Todo).all() is not None:
+        return db.query(Todo).all()
+    raise HTTPException(status_code=404, detail="No data available")
+
+# * API Endpoint: To read the todo using todo id
+@app.get("/read_TODO/{id}", status_code=status.HTTP_200_OK)
+async def readTODOByID(todo_id: int = Path(gt=0), db: Session = Depends(get_db)):
+    ret_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if ret_todo is not None:
+        return ret_todo
+    raise HTTPException(status_code=404, detail=f"TODO not found with id {todo_id}")
 
