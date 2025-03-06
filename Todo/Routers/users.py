14cc5a7ac1f80da5f6947a92d9dd7fe8a193bb9a
Rajnish-J <rajnishofficial02@gmail.com>
2025-03-06 17:12:57 +0530

    FEAT: jwt decode done

--- Todo/Routers/users.py
+++ Todo/Routers/users.py
@@ -6,10 +6,10 @@ from database import SessionLocal
 from sqlalchemy.orm import Session
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 import os
 from dotenv import load_dotenv
-import jwt
+from jose import jwt, JWTError
 
 # * Load environment variables
 load_dotenv()
@@ -18,6 +18,7 @@ ALGORITHM = "HS256"
 
 router = APIRouter(prefix="/user", tags=["User"])
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+ouath2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 # * Dependency to get the database session
 def get_db():
@@ -42,6 +43,19 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
         'exp': datetime.now(timezone.utc) + expires_delta
     }
     return jwt.encode(payload, JWT_SECRET_KEY, algorithm=ALGORITHM)
+
+# * decode JWT Access Token
+def get_current_user(token: Annotated[str, Depends(ouath2_bearer)]):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        username : str = payload.get('sub')
+        user_id : int = payload.get('id')
+        
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate user")
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate user")
 
 # * Request Model for Creating Users
 class UserRequest(BaseModel):
